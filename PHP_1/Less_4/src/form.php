8d8c952135e1dc0005f6178eb56721bdd46e5b3f
mutedalien <chel_c@mail.ru>
2020-01-31 21:12:32 +0300

    Работа с файлами

--- PHP_1/Less_4/src/form.php
+++ PHP_1/Less_4/src/form.php
@@ -2,6 +2,6 @@
 
 <form action="server.php" method="post" enctype="multipart/form-data"> <!-- enctype - тот самый атрибут, указывающий, что надо передать файл на сервер -->
     <p>Выберите файл</p>
-    <input type="file" name="photo"><br><br> <!-- Добавляем случайный тэг (имя) файлу -->
+    <input type="file" name="photo"><br><br> <!-- Добавляем случайный тэг (имя) файлу ("photo")  -->
     <input type="submit" value="Сохранить"> <!-- Рисуем кнопку с типом submit -->
 </form>
