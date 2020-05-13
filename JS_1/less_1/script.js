d4f17bbd9ada7a8b7a015abd526f64111082ca1a
mutedalien <chel_c@mail.ru>
2020-05-13 08:40:59 +0300

    js1_less1

---
+++ JS_1/less_1/script.js
@@ -0,0 +1,16 @@
+
+/*
+alert("Welcome!");
+*/
+
+
+/* alert("Welcome!");
+alert("Welcome!"); */  //текст
+
+/* ctrl + shift + /  -  комментирование */
+
+
+/*ctrl + z - возврат изменений*/
+
+/*ctrl + s - сохранение кода*/
+
